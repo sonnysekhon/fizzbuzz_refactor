a22543192511f65abda20b19b552ec1d33ed0d97
Sonny Sekhon <sonnysekhon@gmail.com>
2015-06-23 20:06:25 +0000

    Indent

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 def fizzbuzz(a,b)
   a.upto(b) do |i|
-  if i % 5 == 0 && i % 3 == 0
-    puts "FizzBuzz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  elsif i % 3 == 0
-    puts "Fizz"
-  else
-    puts i
-  end
+    if i % 5 == 0 && i % 3 == 0
+      puts "FizzBuzz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    elsif i % 3 == 0
+      puts "Fizz"
+    else
+      puts i
+    end
   end
 end
 fizzbuzz(4,60)
